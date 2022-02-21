4e59a4b92fc60abd5fe366bdadab6607ae46a91a
Andre Furquim <afurquim@mcfadyen.com>
2022-02-21 15:56:45 -0300

    hot reloading for store and reducer

--- src/index.js
+++ src/index.js
@@ -19,6 +19,22 @@ ReactDOM.render(
   document.getElementById('root')
 );
 
+/*
+if (module.hot) {
+  module.hot.accept('./App', () => {
+    const NextApp = require('./App').default;
+    ReactDOM.render(
+      <Provider store={store}><NextApp /></Provider>,
+      document.getElementById('root')
+    );
+  });
+
+  module.hot.accecpt('./reducers', () => {
+    const nextRootReducer = require('./reducers').default;
+    store.replacecReducer(nextRootReducer);
+  });
+}
+*/
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals

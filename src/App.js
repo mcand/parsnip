c980d9b6810c888b66d9f0a4820f70527ae80210
Andre Furquim <afurquim@mcfadyen.com>
2022-02-21 12:13:17 -0300

    Add redux as state management

--- src/App.js
+++ src/App.js
@@ -1,30 +1,23 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
 import TasksPage from './components/TasksPage.js';
-
-const mockTasks = [
-  {
-    id: 1,
-    title: 'Learn Redux',
-    description: 'The store, actions, and reducers, oh my!',
-    status: 'In Progress',
-  },
-  {
-    id: 2,
-    title: 'Peace on Earth',
-    description: 'No big deal',
-    status: 'In Progress',
-  },
-];
 
 class App extends Component {
   render() {
     return (
-      <div className="main-content">
-        <TasksPage tasks={mockTasks} />
+     <div className="main-content">
+        <TasksPage tasks={this.props.tasks} />
       </div>
     );
   }
+
 }
 
-export default App;
+function mapStateToProps(state) {
+  return {
+    tasks: state.tasks
+  }
+}
+
+export default connect(mapStateToProps)(App);
 

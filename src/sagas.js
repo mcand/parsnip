e64345f2761af6955200e7f6d22959dd464b3e2a
Andre Furquim <afurquim@mcfadyen.com>
2022-02-22 18:53:10 -0300

    using takelatest

--- src/sagas.js
+++ src/sagas.js
@@ -1,30 +1,22 @@
-import { call, fork, put, take } from 'redux-saga/effects';
+import { call, put, takeLatest } from 'redux-saga/effects';
 import * as api from './api';
 
 export default function* rootSaga() {
-  yield fork(watchFetchTasks);
-  yield fork(watchSomethingElse);
+  yield takeLatest('FETCH_TASKS_STARTED', fetchTasks);
 }
 
-function* watchFetchTasks() {
-  while (true) {
-    yield take('FETCH_TASKS_STARTED');
-    try {
-      const { data } = yield call(api.fetchTasks);
-      yield put({
-        type: 'FETCH_TASKS_SUCCEEDED',
-        payload: { tasks: data }
-      });
-    } catch (e) {
-      yield put({
-        type: 'FETCH_TASKS_FAILED',
-        payload: { error: e.message }
-      });
-    }
+function* fetchTasks() {
+  try {
+    const { data } = yield call(api.fetchTasks);
+    yield put({
+      type: 'FETCH_TASKS_SUCCEEDED',
+      payload: { tasks: data }
+    });
+  } catch (e) {
+    yield put({
+      type: 'FETCH_TASKS_FAILED',
+      payload: { error: e.message }
+    });
   }
-}
-
-function* watchSomethingElse() {
-  console.log('watching something else!');
 }
 
